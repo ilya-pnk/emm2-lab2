caab0189f7290cd020c37b8d11eb7b6ef9cb1aed
ilya <ilya-panko@mail.ru>
2024-11-26 16:42:48 +0700

    GARCH(3,0)

--- emm2-lab2.R
+++ emm2-lab2.R
@@ -29,3 +29,22 @@ plot(h, type = "l", main = "График волатильности GARCH(1,0)",
 garch1 <- garch(sigma, order = c(1, 0))
 summary(garch1)
 
+# Задание 3: GARCH(3,0)
+a0_3 = 0.1
+a1_3 = 0.5
+a2_3 = 0.3
+a3_3 = 0.1
+n=1100
+
+epsilon = rnorm(n)
+
+# Инициализация массивов
+h = numeric(n)
+sigma = numeric(n)
+
+# Моделирование GARCH(3,0)
+for (t in 4:n) {
+  h[t] <- sqrt(a0_3 + a1_3 * h[t-1]^2 + a2_3 * h[t-2]^2 + a3_3 * h[t-3]^2)
+  sigma_garch30[t] <- h[t] * epsilon[t]
+}
+
